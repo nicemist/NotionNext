17033e07f58c739bdfb4d23153229549ef5856f5
Sundrm <2056217382@qq.com>
2023-05-27 21:24:14 +0800

    1

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'source.unsplash.com',
       'p1.qhimg.com',
-      'webmention.io'
+      'webmention.io',
+      's1.vika.cn'
     ]
   },
   // 默认将feed重定向至 /public/rss/feed.xml
